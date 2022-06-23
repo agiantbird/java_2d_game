ab27b46b828acb0c08008aa37057dce6a917d04e
Rich Hoyt <rhoyt10@gmail.com>
2022-06-23 13:49:15 -0400

    Add game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 import java.awt.Color;
 
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
 // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile, default size of sprites/map tiles
@@ -19,11 +19,25 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768px
     final int screenHeight = tileSize * maxScreenRow; // 576px
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         // drawing from this component will be done in an offscreen painting buffer,
         // this can improve the game's rendering performance
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while(gameThread != null) {
+            System.out.println("Game loop is running");
+        }
     }
 }
