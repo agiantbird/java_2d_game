e4af5dd0a42774afc47a691cf80203b9e710fe1e
Rich Hoyt <rhoyt10@gmail.com>
2022-08-03 13:10:44 -0400

    Add title screen

--- src/main/UI.java
+++ src/main/UI.java
@@ -15,6 +15,7 @@ public class UI {
     int messageCounter = 0;
     public boolean gameFinished = false;
     public String currentDialogue = "";
+    public int commandNum = 0;
 
     public UI(GamePanel gp) {
         this.gp = gp;
@@ -44,7 +45,10 @@ public class UI {
 //        g2.setFont(purisaB);
 //        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g2.setColor(Color.WHITE);
-
+        // TITLE STATE
+        if(gp.gameState == gp.titleState) {
+            drawTitleScreen();
+        }
         // PLAY STATE
         if(gp.gameState == gp.playState) {
             // Do playState stuff later
@@ -56,6 +60,55 @@ public class UI {
         // DIALOGUE STATE
         if(gp.gameState == gp.dialogueState) {
             drawDialogueScreen();
+        }
+    }
+
+    public void drawTitleScreen() {
+        // BACKGROUND COLOR
+        g2.setColor(new Color(0, 0, 0));
+        g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
+        // TITLE NAME
+        g2.setFont(g2.getFont().deriveFont(Font.BOLD,96F));
+        String text = "Java 2D Game";
+        int x = getXForCenteredText(text);
+        int y = gp.tileSize * 3;
+        // SHADOW
+        g2.setColor(Color.gray);
+        g2.drawString(text, x+5, y+5);
+        // MAIN TEXT COLOR
+        g2.setColor(Color.white);
+        g2.drawString(text, x, y);
+
+        // MAIN CHARACTER IMAGE
+        x = (gp.screenWidth / 2) - gp.tileSize;
+        y += gp.tileSize * 2;
+        g2.drawImage(gp.player.down1, x, y, gp.tileSize * 2, gp.tileSize * 2, null);
+
+        // MENU
+        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 48F));
+
+        text = "NEW GAME";
+        x = getXForCenteredText(text);
+        y += gp.tileSize * 3.5;
+        g2.drawString(text, x, y);
+        if(commandNum == 0) {
+            g2.drawString(">", x - gp.tileSize, y);
+        }
+
+        text = "LOAD GAME";
+        x = getXForCenteredText(text);
+        y += gp.tileSize;
+        g2.drawString(text, x, y);
+        if(commandNum == 1) {
+            g2.drawString(">", x - gp.tileSize, y);
+        }
+
+        text = "QUIT";
+        x = getXForCenteredText(text);
+        y += gp.tileSize;
+        g2.drawString(text, x, y);
+        if(commandNum == 2) {
+            g2.drawString(">", x - gp.tileSize, y);
         }
     }
 
