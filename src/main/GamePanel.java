4de412dc77c73afee0490d4fd06fa3ddf6139c0e
Rich Hoyt <rhoyt10@gmail.com>
2022-08-17 12:36:51 -0400

    Add monster class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -47,6 +47,7 @@ public class GamePanel extends JPanel implements Runnable {
     public Player player = new Player(this, keyH);
     public Entity obj[] = new Entity[10];
     public Entity npc[] = new Entity[10];
+    public Entity monster[] = new Entity[20];
     ArrayList<Entity> entityList = new ArrayList<>();
 
     // GAME STATE
@@ -70,6 +71,7 @@ public class GamePanel extends JPanel implements Runnable {
     public void setupGame() {
         aSetter.setObject();
         aSetter.setNPC();
+        aSetter.setMonster();
         //playMusic(0);
         gameState = titleState;
     }
@@ -117,6 +119,12 @@ public class GamePanel extends JPanel implements Runnable {
                     npc[i].update();
                 }
             }
+            // MONSTER
+            for(int i = 0; i < monster.length; i++) {
+                if(monster[i] != null) {
+                    monster[i].update();
+                }
+            }
         }
 
         if(gameState == pauseState) {
@@ -155,6 +163,12 @@ public class GamePanel extends JPanel implements Runnable {
             for(int i = 0; i < obj.length; i++) {
                 if(obj[i] != null) {
                     entityList.add(obj[i]);
+                }
+            }
+
+            for(int i = 0; i < monster.length; i++) {
+                if(monster[i] != null) {
+                    entityList.add(monster[i]);
                 }
             }
             // SORT ENTITY LIST FOR DRAW ORDER
